a21f5f8408da46634d17ec0294cdf7702da0caf5
Elvin Bashirli <ebashirli@gmail.com>
2024-09-12 11:53:53 +0400

    add mobile nav

---
+++ components/modile-nav.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { CiMenuFries } from "react-icons/ci";
+import Logo from "@/components/logo";
+import { links } from "@/lib/constants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/router";
+
+export default function MobileNav() {
+  const pathname = usePathname();
+
+  return (
+    <Sheet>
+      <SheetTrigger className="flex justify-center items-center ">
+        <CiMenuFries className="text-[32px]" />
+      </SheetTrigger>
+      <SheetContent className="flexk flex-col bg-white ">
+        <div className="mt-24 mb-40 flex justify-center">
+          <Logo />
+        </div>
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {links.map((link) => (
+            <Link
+              href={`#${link.path}`}
+              key={link.name.en}
+              className={`${
+                link.path === pathname
+              } capitalize text-xl hover:text-accent transition-all`}
+            >
+              {link.name.az}
+            </Link>
+          ))}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
+}
--- components/nav.tsx
+++ components/nav.tsx
@@ -1,15 +1,7 @@
 "use client";
+import { links } from "@/lib/constants";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
-const links = [
-  { name: { en: "courses", az: "kurslar" }, path: "#courses" },
-  { name: { en: "about us", az: "haqqımızda" }, path: "#about" },
-  { name: { en: "teachers", az: "müəllimlər" }, path: "#teachers" },
-  { name: { en: "pricing", az: "qiymətlər" }, path: "#pricing" },
-  { name: { en: "careers", az: "karyera" }, path: "#careers" },
-  { name: { en: "blogs", az: "bloqlar" }, path: "#blogs" },
-];
 
 export default function Nav() {
   const pathname = usePathname();
