52201367c224c077b4d0ca2b2fc4fa29b94b563f
Elvin Bashirli <ebashirli@gmail.com>
2024-09-13 22:53:32 +0400

    finish testimonials section

--- components/testimonial.tsx
+++ components/testimonial.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Testimonial() {
   return (
-    <section className="px-16 xl:px-32 mb-96 xl:h-[calc(100vh-64px)]  flex flex-col gap-16  ">
+    <section className="px-16 xl:px-32 mb-96 xl:h-[calc(100vh-64px)]  flex flex-col gap-16 ">
       <div className="text-center xl:text-left">
         <h3 className="text-[#b0b0c0] text-lg font-bold uppercase leading-[34.20px]">
           top studiying
@@ -12,16 +12,16 @@ export default function Testimonial() {
         </h4>
       </div>
       <div className="grid gap-16 xl:grid-cols-2 grow ">
-        <div className="relative bg-[#fceae3] rounded-[40px] max-w-[450px] h-full grow">
+        <div className="relative bg-[#fceae3] rounded-[40px] max-w-[calc(100%/1.4)] max-h-full grow">
           <Image
             src="/testimonials/s1.png"
             alt=""
             fill
-            className="object-contain rounded-[40px] "
+            className="object-cover rounded-[40px] "
           />
         </div>
-        <div className="flex gap-2 flex-col justify-center ">
-          <h5 className="font-bold">Martin Watson</h5>
+        <div className="flex gap-2 flex-col justify-center max-w-[400px]">
+          <h5 className="font-bold text-2xl">Martin Watson</h5>
           <div className="flex items-end gap-2">
             <div className="h-10 w-10 relative ">
               <Image
@@ -33,7 +33,7 @@ export default function Testimonial() {
             </div>
             <p>B2 course student</p>
           </div>
-          <p>
+          <p className="text-lg">
             There is no way I could have made the same progress learning Spanish
             without using Lingua. The best part is now learning Spanish has
             become one of my biggest hobbies.
