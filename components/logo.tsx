41e0b26a04b95a279aedd40e9ad0078751e74023
Elvin Bashirli <ebashirli@gmail.com>
2024-09-27 09:32:15 +0400

    fix logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,12 +1,25 @@
+import { Montserrat } from "next/font/google";
 import Link from "next/link";
+
+const montserrat = Montserrat({
+  weight: ["900"],
+  subsets: ["latin"],
+});
 
 export default function Logo() {
   return (
-    <Link href="/" className="flex">
-      <h1 className="font-bold flex items-center justify-center space-x-1 text-3xl text-[#181a31] bold ">
+    <Link href="/" className={`${montserrat.className} flex`}>
+      <h1 className="font-bold font flex items-center justify-center space-x-1 text-3xl text-[#181a31] bold ">
         {/* <span className="h-7 w-7 bg-[#ffa500] rounded-full" /> */}
-        <span className="">ONE academy</span>
-        <span className="text-accent">.</span>
+        <span className="uppercase text-primary-200 flex gap-4">
+          <span className="border-b-8 border-primary-200">one</span>
+          <span className="grid relative">
+            <span>academy</span>
+            <span className="text-[9.8px] absolute -bottom-[14.5px]  ">
+              for becoming the first one
+            </span>
+          </span>
+        </span>
       </h1>
     </Link>
   );
